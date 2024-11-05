ce15f3e1c76c866e2e0c4016e047eb851ecf968f
Sanjuk526392 <sanjukadapatti36@gmail.com>
2024-11-05 10:22:18 +0530

    Add files via upload

---
+++ escapesequances.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("hello world\n");
+    printf("the world is \t big");
+    printf("the earth \bis Blue");
+    printf("the world has 7 continent");
+    printf("the india has 28 states/(india)");
+    printf("india is a \'big\'country");
+}
