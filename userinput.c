2b2489208f69c1f0458bbf0fab26e3f40b19b37a
Sanjuk526392 <sanjukadapatti36@gmail.com>
2024-11-05 12:58:17 +0530

    Add files via upload

---
+++ userinput.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    int b;
+    int c;
+    printf("enter two numbers:");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    c=a+b;
+    printf("sum of two nos are as follows:");
+    printf("%d",c);
+}
