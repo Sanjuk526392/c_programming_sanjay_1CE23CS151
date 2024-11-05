f71fde170c2b2125a7c701197c4b8dc801fe1ec4
Sanjuk526392 <sanjukadapatti36@gmail.com>
2024-11-05 10:56:22 +0530

    Add files via upload

---
+++ operation(datatypes).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int a=10;
+    printf("%d",a);
+
+    float b = 5.75;
+    double c = 19.99;
+    printf("%f\n", b);
+    printf("%lf\n", c);
+    char e='a';
+    printf("%c",e);
+    char z[]="city wbfwbfkewbkb";
+    printf("%s\n",z);
+
+}
