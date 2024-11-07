3f13e7dcd5a66e1f307606349591b677505c284d
Sanjuk526392 <sanjukadapatti36@gmail.com>
2024-11-07 15:36:02 +0530

    Add files via upload

---
+++ danglingptr.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+    int* ptr =(int*)malloc(sizeof(int));
+    *ptr=10;
+    printf("before free %d\n",*ptr);
+    free(ptr);
+    printf("after free %d",*ptr);
+    return 0;
+}
