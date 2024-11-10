e3ae13c0ed834f014da28aa666ca54437564f867
Sanjuk526392 <sanjukadapatti36@gmail.com>
2024-11-10 10:26:10 +0530

    Add files via upload

---
+++ transpose.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int arr[2][2]={
+        {2,3},
+        {4,5}};
+    for(int i=0; i<2; i++){
+        for(int j=0; j<2; j++){
+                printf("%d ",arr[j][i]);
+            }
+            printf("\n");
+        }
+        return 0;
+    }\\8=\=\68\=\8=\68=6\8\\=7\uu\7=\=7\=po\\u[u]l]ul[pjk-u-]]6kk[opupopo[oj[opoooou[oo[kop]okj]]]]mjmmmjupppp55222
+    
